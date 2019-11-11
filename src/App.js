0e59398e575d5b02cc1b4ae9abe3fd567300c813
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-11 14:21:42 +0000

    changees

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
+// import addToDo from './addToDo';
 
 let array = ['wash car', 'do dishes']
 
 const App = () => {
-  function handleClick(e) {
-    console.log('button click')
+  function addToDo(e) {
+    array.push(e)
   }
   return (
     <>
     <h1>Today's Tasks</h1>
     <li>A task</li>
     <li>{array[array.length - 1]}</li>
-    <input placeholder="enter your task here"/> <button onClick={handleClick}>Submit</button>
+    <input placeholder="enter your task here"/> <button onClick={addToDo}>Submit</button>
     </>
   );
 };
--- src/addToDo.js
+++ src/addToDo.js
@@ -1,5 +1,8 @@
-import React from 'react';
+// import React from 'react';
+// import array from './App.js';
 
-const addToDo = () => {
-    
-}
+// const addToDo = (e) => {
+//     array.push(e);
+// }
+
+// export default addToDo;
