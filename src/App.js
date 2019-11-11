dec492d4e7174f20f223a21457aef8cc67a570d5
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-11 12:56:51 +0000

    added a button

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
-// handle forms?
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+    <h1>Today's Tasks</h1>
+    <li>A task</li>
+    <input placeholder="enter your task here"/> <button>Submit</button>
+    </>
+  );
+};
+
+export default App;
